c653bb9cea2df9ed92312d106bdbdaab7f82a588
jack-techexec <jack@digitaltechexec.com>
2020-05-14 16:51:36 +1000

    works lol, dumb comments still in here

--- birthday.py
+++ birthday.py
@@ -1,8 +1,11 @@
 import time
 import ast
+import random
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
 try:
+#attempt to import settings
     file = open("settings.json", "r")
     contents = file.read()
     dictionary = ast.literal_eval(contents)
@@ -14,19 +17,23 @@ try:
     
     
 except:
+    #error in getting settings
     print("Error parsing settings.json")
     time.sleep(delay)
     exit()
 else:
-    time.sleep(delay)  # Let the user actually see something!
-# Optional argument, if not specified will search path.
-
+    
+    #sleep cause why not
+    time.sleep(delay) 
+  
+#define driver and load intial page
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
-# driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=r'C:\Users\raow2\Documents\BirthdayPy\chromedriver_win32\chromedriver.exe')
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromeDriverLocation)
 driver.get('http://m.facebook.com/')
-time.sleep(delay)  # Let the user actually see something!
+time.sleep(delay) 
+
+#login
 email = driver.find_element_by_name('email')
 email.send_keys(userEmail)
 password = driver.find_element_by_name('pass')
@@ -35,15 +42,58 @@ password.send_keys(userPass)
 login = driver.find_element_by_name('login')
 login.click()
 time.sleep(delay)
-driver.get("https://m.facebook.com/events/calendar/birthdays/")
-time.sleep(delay)  # Let the user actually see something!
 
-# todaysBirthdays = driver.find_element_by_xpath("(//ul)[1]")
-# for element in  driver.find_elements_by_class_name('_52jh _5at0 _592p'):
-for element in  driver.find_elements_by_xpath("(//ul)[1]//div//p"):
-    print(element.text)
+#navigate to bday list page
+driver.get("https://m.facebook.com/events/calendar/birthdays/")
+time.sleep(delay)  
+
+#get list of names and urls for their profile pages
+names = []
+urls = []
+for element in  driver.find_elements_by_xpath("(//ul)[1]"):
+    name = element.find_element_by_xpath("//a//p[1]")
+    names.append(name.text)
+    url = element.find_element_by_xpath("//a")
+    urls.append(url.get_attribute('href')) 
+    bdayMessage = "bday message"
+    with open("birthday.cfg") as f:
+        lines = f.readlines()
+        bdayMessage = random.choice(lines)
+    textinput = element.find_element_by_xpath("//textarea")
+    if "$name" in bdayMessage:
+        textinput.send_keys(bdayMessage.replace("$name",name.text.split(' ', 1)[0]))
+    elif "$name" not in bdayMessage:
+        textinput.send_keys(bdayMessage)
+    
+    time.sleep(delay/2)
+
+    textsend = element.find_element_by_xpath("//button") name.text.split(' ', 1)[0]
+    textsend.click()
     time.sleep(delay)
-print('logged in successfully')
+  
+
+
+
+
+
+
+#goto each persons page and write happy birthday message of sorts!
+# for x in urls:
+#     driver.get(x)
+#     print(names[urls.index(x)])
+#     time.sleep(delay)
+#     composebutton = driver.find_element_by_xpath("//*[contains(text(), 'Write on')]")
+#     composebutton.click()
+#     time.sleep(delay)
+#     composeinput = driver.find_element_by_xpath("//textarea[1]")
+#     composeinput.send_keys("Happy birthday!")
+#     time.sleep(delay)
+
+
+
+
+
+print('ran successfully')
 time.sleep(1)
 driver.quit()
 exit()
--- readme.md
+++ readme.md
@@ -9,9 +9,9 @@ Download [ChromeDriver](chromedriver.chromium.org/downloads)
 
 Install [Selenium](https://selenium-python.readthedocs.io/installation.html#downloading-python-bindings-for-selenium) (only need step 1.2)
 
-Change samplesettings.json to settings.json
-Fill out account information under settings.json
-Add chromedriver location
-Pick a suitable delay between actions, would recommend around 5 seconds. (will add proper waiting for page to load etc if i get bored)
+Change samplesettings.json to settings.json<br/>
+Fill out account information under settings.json<br/>
+Add chromedriver location<br/>
+Pick a suitable delay between actions, would recommend around 5 seconds. (will add proper waiting for page to load etc if i get bored)<br/>
 
 Then use a scheduler to make this run every day (ensure this only runs once, will repost on walls if ran more than once).
